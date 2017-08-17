ad01c4ae923fbc86ebdfd61d61f19fe7f728e936
pangxin <pangxin@pangxindeMacBook-Pro.local>
2017-08-17 09:21:34 +0800

    fix;

--- src/tsung/ts_http_common.erl
+++ src/tsung/ts_http_common.erl
@@ -129,8 +129,8 @@ http_body(Method,#http_request{url=URL, version=Version,
     ?LOGF("Headers~n-------------~n~s~n",[H],?DEB),
 	Content2 = http_uri:encode(binary_to_list(Content)),
     ?LOGF("9902 Content=~p,Content2=~p~n", [Content,Content2], ?INFO),
-    H = list_to_binary([H, Content2]),
-	?LOGF("9903 H=~p~n", [H], ?INFO),
+    H2 = list_to_binary([H, Content2]),
+	?LOGF("9903 H=~p~n", [H2], ?INFO),
 	list_to_binary([H, Content2]).
 
 %%----------------------------------------------------------------------
